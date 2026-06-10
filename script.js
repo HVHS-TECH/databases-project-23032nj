f1cdcf7ed110d03856983d81926b9dc9d9f2c14c
23032nj <23032nj@hvhs.school.nz>
2026-06-10 15:14:35 +1200

    setting up user registration

--- script.js
+++ script.js
@@ -8,21 +8,89 @@ console.log("Running Nia's Games")
 //Constants
 
 
-
 //Variables
+let userLoggedIn = false
+let userFormSubmitted = false
+let userRegistered = false
 
+let userDisplayName;
+let userEmail;
+let userUID;
+let userProfilePicture
 
-
+let userName;
+let userAge;
 /**********************************
 // authenticateLogin()
 ***********************************/
 
+function authenticateLogin() {
+    console.log("Logging in user")
+    authenticationListner = firebase.auth().onAuthStateChanged(handleLogin)
+}
 
+function handleLogin(_user) {
+    if (userLoggedIn == false) {
+        if(_user) {
+            GLOBAL_user = _user;
+            console.log("User is logged in")
+            userDisplayName = GLOBAL_user.displayName
+            userEmail = GLOBAL_user.email
+            userUID = GLOBAL_user.uid
+            userProfilePicture = GLOBAL_user.photoURL
+            console.log(userDisplayName)
+            console.log(userEmail)
+            console.log(userUID)
+            console.log(userProfilePicture)
+            userLoggedIn = true
+        } else {
+            console.log("User is not logged in, logging the user in now")
+            popupLogin();
+        }
+    }
+}
 
+function popupLogin() {
+    var provider = new firebase.auth.GoogleAuthProvider();
 
+    firebase.auth().signInWithPopup(provider).then((result) => {
+    GLOBAL_user = result.user;
+    console.log("User is logged in")
+    userDisplayName = GLOBAL_user.displayName
+    userEmail = GLOBAL_user.email
+    userUID = GLOBAL_user.uid
+    userProfilePicture = GLOBAL_user.photoURL
+    console.log(userDisplayName)
+    console.log(userEmail)
+    console.log(userUID)
+    console.log(userProfilePicture)
+    userLoggedIn = true
+})
+}
 
 /**********************************
 // submitUserDetailsForm()
 ***********************************/
 
+function submitUserDetailsForm() {
+    //collect and store the users username and age
+    if(userLoggedIn == true) {
+        const userName = document.getElementById("name").value;
+        const userAge = document.getElementById("age").value;
+        console.log(userName)
+        console.log(userAge)
 
+        saveUserInfo()
+        userFormSubmitted = true;
+    } else {
+        alert("Please log in before registering")
+    }
+}
+
+/**********************************
+// saveUserInfo()
+***********************************/
+
+function saveUserInfo() {
+    console.log("running saveUserInfo")
+}
---
+++ setup.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDpx2n8Un495Cdq2U07o_gX9Dv8BjzUmvQ",
+  authDomain: "comp-2026-nia-jobanputra.firebaseapp.com",
+  databaseURL: "https://comp-2026-nia-jobanputra-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "comp-2026-nia-jobanputra",
+  storageBucket: "comp-2026-nia-jobanputra.firebasestorage.app",
+  messagingSenderId: "94059285932",
+  appId: "1:94059285932:web:32ea40009f494c429e3e09"
+};
+  //Set up the connection to your Firebase Realtime Database
+firebase.initializeApp(firebaseConfig);
+
+console.log(firebase);
