88f9093fea34ca4fa1c48eadd0c34b2f63a3fe26
23032nj <23032nj@hvhs.school.nz>
2026-06-22 10:04:29 +1200

    adding only save higher scores code to block breaker

--- js/block_game.js
+++ js/block_game.js
@@ -194,9 +194,25 @@ function blockHit() {
     blockCreateRound = 0;
     //revealing the endscreen
     gameEnd.style.display = "block";
-      firebase.database().ref('/gameScores/blockBreaker/'+userUID).update({
-      userScore: score
-    })
+
+    //database writes
+    firebase.database().ref('/gameScores/blockBreaker/' + userUID).once('value', displayScore, fb_readError);
+
+    function displayScore(snapshot) {
+        var userPastScore = snapshot.val().userScore;
+        console.log(userPastScore)
+        if (userPastScore < score) {
+            firebase.database().ref('/gameScores/blockBreaker/' + userUID).update({
+                userScore: score
+            })
+        }
+    }
+
+    function fb_readError(error) {
+        console.log("There was an error reading the message");
+        console.error(error);
+    }
+
   }
 
 /*******************************************************/
