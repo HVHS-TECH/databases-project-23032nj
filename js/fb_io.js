ea5d87c073f27abed2a4b070cec6d02dc17fd063
23032nj <23032nj@hvhs.school.nz>
2026-06-23 12:07:09 +1200

    adding null check for writing scores

---
+++ js/fb_io.js
@@ -0,0 +1,4 @@
+    function fb_readError(error) {
+        console.log("There was an error reading the message");
+        console.error(error);
+    }
--- js/writeScores.js
+++ js/writeScores.js
@@ -3,26 +3,38 @@ function fb_writeGeoDashScore() {
     firebase.database().ref('/gameScores/geoDash/' + userUID).once('value', displayScore, fb_readError);
 
     function displayScore(snapshot) {
-        var userPastScore = snapshot.val().userScore;
+        var checkUserPastScore = snapshot.val();
+        var userPastScore;
+        if(checkUserPastScore == null) {
+            console.log("past score is null")
+            userPastScore = 0
+        }
+        else {
+            userPastScore = snapshot.val().userScore;
+        }
         console.log(userPastScore)
-        if (userPastScore < score) {
+        if (userPastScore < score || userPastScore == 0) {
             firebase.database().ref('/gameScores/geoDash/' + userUID).update({
                 userScore: score
             })
         }
     }
 
-    function fb_readError(error) {
-        console.log("There was an error reading the message");
-        console.error(error);
-    }
 }
 
 function fb_writeBlockBreakerScore() {
     firebase.database().ref('/gameScores/blockBreaker/' + userUID).once('value', displayScore, fb_readError);
 
     function displayScore(snapshot) {
-        var userPastScore = snapshot.val().userScore;
+        var checkUserPastScore = snapshot.val();
+        var userPastScore;
+        if(checkUserPastScore == null) {
+            console.log("past score is null")
+            userPastScore = 0
+        }
+        else {
+            userPastScore = snapshot.val().userScore;
+        }
         console.log(userPastScore)
         if (userPastScore < score) {
             firebase.database().ref('/gameScores/blockBreaker/' + userUID).update({
@@ -30,9 +42,9 @@ function fb_writeBlockBreakerScore() {
             })
         }
     }
+}
 
-    function fb_readError(error) {
-        console.log("There was an error reading the message");
-        console.error(error);
-    }
+function fb_readError(error) {
+    console.log("There was an error reading the message");
+    console.error(error);
 }
