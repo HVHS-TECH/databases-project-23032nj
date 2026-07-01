ff38b18cad5b272f53bc3b32e2503971549247d5
23032nj <23032nj@hvhs.school.nz>
2026-07-01 15:16:51 +1200

    putting html files into a folder

--- js/script.js
+++ js/script.js
@@ -14,9 +14,9 @@ function checkUser() {
 function displayUsers(snapshot) {
     var currentUser = snapshot.val();
     if (currentUser == null) { //if there is no currentUser, dbData will be null, therefore they're new to the interface
-        window.location.href = "user-signup.html";
+        window.location.href = "../HTML/user-signup.html";
     }
     else { //user is already in the database, they can go straight to game selection page
-        window.location.href = "game-selection.html";
+        window.location.href = "../HTML/game-selection.html";
     }
 }
